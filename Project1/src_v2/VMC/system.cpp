a1a49e27a2be36520dd8ac525ffe45994f7765a6
gunnarlan <lange.gunn@gmail.com>
2018-02-28 19:32:08 +0100

    Added energy

--- Project1/src_v2/VMC/system.cpp
+++ Project1/src_v2/VMC/system.cpp
@@ -9,9 +9,22 @@ System::System()
     next_r = new Eigen::MatrixXd(Parameters::dimension,Parameters::N);
     distance.resize(Parameters::N,Parameters::N);
     next_distance.resize(Parameters::N,Parameters::N);
+    kinetic_energy=0;
+    potential_energy=0;
+    wavefunction_probability=0;
+    wavefunction_value=get_wavefunction();
+
+    if(Parameters::a !=0){
+        System::make_move = &System::make_move_and_update_interacting;
+    }
+    else{
+        System::make_move=&System::make_move_and_update_non_interacting;
+    }
 
     //Temp variables
-    //temp_r.resize(Parameters::dimension);
+    temp_r.resize(Parameters::dimension);
+    temp_r2.resize(Parameters::dimension);
+
     //temp_value = 0;
     //temp_value2 = 0;
 
@@ -48,15 +61,28 @@ void System::update(){
     }
 }
 
+
 void System::make_move_and_update(const int move){
+    (this->*make_move)(move);
+}
+
+
+void System::make_move_and_update_non_interacting(const int move){
     //Makes a random move
     for(int i = 0; i<dimension; i++){
         next_r->coeffRef(i,move) += dx*((double)rand()/RAND_MAX - 0.5);
     }
+
+}
+
+void System::make_move_and_update_interacting(const int move){
+    //Makes a random move
+    for(int i = 0; i<dimension; i++){
+        next_r->coeffRef(i,move) += dx*((double)rand()/RAND_MAX - 0.5);
+    }
+
     double temp_value = 0;
     //Updates the distance matrix after move
-
-    /*
     for(int i = 0;i<N;i++){
         if(i != move){
             temp_value = (r->col(move)- r->col(i)).norm();
@@ -64,8 +90,6 @@ void System::make_move_and_update(const int move){
             next_distance(move,i) = temp_value;
         }
     }
-    */
-
 }
 
 double System::check_acceptance_and_return_energy(int move){
@@ -74,7 +98,7 @@ double System::check_acceptance_and_return_energy(int move){
 
     //If r is less than the acceptance prob, r is updated to the new r
     if(temp_value <= get_probability_ratio(move)){
-
+        update_wavefunction(move);
         r = next_r;
         //update();
         //distance = next_distance;
@@ -120,6 +144,27 @@ double System::get_probability(){
     return temp_value*temp_value;
 }
 
+double System::calculate_energy_noninteracting(){
+    for(int i=0; i<N;i++){
+        temp_r=r->col(i);
+        temp_r2=r->col(i);
+        potential_energy += 0.5*omega*temp_r.squaredNorm();
+        for(int j=0; j<dimension;j++){
+            temp_r2(j)+=h;
+            kinetic_energy+=exp(phi_exponant(temp_r2-temp_r));
+            temp_r2(j)-=2*h;
+            kinetic_energy+=exp(phi_exponant(temp_r2-temp_r));
+        }
+    }
+    kinetic_energy *= 1/(2.0*h*h);
+    return kinetic_energy+potential_energy;
+
+}
+
+void System::update_wavefunction(const int move){
+    wavefunction_value*=exp(phi_exponant(next_r->col(move))-phi_exponant(r->col(move)));
+
+}
 
 double System::get_local_energy(){
     return 0;
--- Project1/src_v2/VMC/system.h
+++ Project1/src_v2/VMC/system.h
@@ -22,22 +22,32 @@ public:
     const Eigen::MatrixXd get_position() const;
 
     void make_grid(double m_alpha);
-    void make_move_and_update(int move);
+    void make_move_and_update_non_interacting(const int move);
+    void make_move_and_update_interacting(const int move);
+    void make_move_and_update(const int move);
     void update();
     double check_acceptance_and_return_energy(int);
 
+
+    void update_wavefunction(const int move);
+    double calculate_energy_noninteracting();
 private:
     //Saves all the variables from the parameters to save time
     const int N = Parameters::N;
     const int dimension = Parameters::dimension;
     const double beta = Parameters::beta;
     const double dx = Parameters::dx;
+    const double omega = Parameters::omega;
+    const double h=1e-8;
+
+    void (System::*make_move)(const int);
 
     //Vector and double used for holding temp values
-    //Eigen::VectorXd temp_r;
+    Eigen::VectorXd temp_r;
+    Eigen::VectorXd temp_r2;
     //double temp_value;
     //double temp_value2;
-    Eigen::MatrixXd *r_temp_pointer;
+
 
 
     //Functions for calulations involving wavefunction
@@ -45,14 +55,14 @@ private:
     double wavefunction_value;
     double wavefunction_probability;
     double local_energy;
+    double kinetic_energy;
+    double potential_energy;
 
     double phi_exponant(const Eigen::VectorXd &r);
     double get_wavefunction();
     double get_probability_ratio(int move);
     double get_probability();
     double get_local_energy();
-
-    void update_wavefunction();
     void update_probability_ratio();
 
 
