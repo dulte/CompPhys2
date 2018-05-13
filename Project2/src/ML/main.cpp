dd7205853b3d55cc400e212d6d4b9d770f002d8c
Daniel <daniel.heinesen@sf-nett.no>
2018-05-13 16:09:52 +0200

    Grad plots

--- Project2/src/ML/main.cpp
+++ Project2/src/ML/main.cpp
@@ -15,30 +15,41 @@ int main(int argc, char *argv[])
     //Reads the parameter file
     Parameters::read_parameters("../input/parameters.txt");
 
-    Eigen::ArrayXd test_parameters = 0.01*Eigen::ArrayXd::Random(Parameters::P*Parameters::dimension + Parameters::N + Parameters::P*Parameters::dimension*Parameters::N);
+    /*Eigen::ArrayXd test_parameters = 0.01*Eigen::ArrayXd::Random(Parameters::P*Parameters::dimension + Parameters::N + Parameters::P*Parameters::dimension*Parameters::N);
 
-    std::cout << test_parameters << std::endl;
     std::cout << "---------------" << std::endl;
-    //test_parameters << -.1,.0,-.31,.10,.76,.69,-.1,.3;
 
     System * system = new System();
     Simulation * simulation = new Simulation(system);
 
     Eigen::ArrayXd done = simulation->stochastic_descent(test_parameters);
 
-
     std::cout <<  test_parameters - done << std::endl;
 
 
-    double dxs[6] = {1,1,0.5,0.1,0.05,0.01};
+    delete simulation;
+    delete system;*/
 
-    for(int i = 0;i<6;i++){
-        Parameters::dx = dxs[i];
-        simulation->run(0,done);
+
+
+    double Ns[5] = {5,4,3,2,1};
+    double rates[4] = {0.5,0.1,0.05,0.01};
+
+    for(int i = 0;i<4;i++){
+        for(int j = 0; j<5;j++){
+            Parameters::N = Ns[j];
+            Parameters::learning_rate = rates[i];
+            System * system = new System();
+            Simulation * simulation = new Simulation(system);
+            Eigen::ArrayXd test_parameters = 0.01*Eigen::ArrayXd::Random(Parameters::P*Parameters::dimension + Parameters::N + Parameters::P*Parameters::dimension*Parameters::N);
+            std::cout << "ehi " << Parameters::N << std::endl;
+            Eigen::ArrayXd done = simulation->stochastic_descent(test_parameters);
+            delete simulation;
+            delete system;
+        }
+
     }
 
-    delete simulation;
-    delete system;
 
     return 0;
 }
